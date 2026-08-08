95af2f3e285996762d89676164f087476fbcdd6e
lfarssi <medlfarssi10@gmail.com>
2026-08-08 09:08:10 +0100

    improving gesture handling

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
 import 'package:flutter/material.dart';
+import 'game_board.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: '2048 Game',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
