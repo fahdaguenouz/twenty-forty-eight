e2d26afedbcea7589a4c0f22143f20a76708c0f2
lfarssi <medlfarssi10@gmail.com>
2026-08-13 06:44:10 +0100

    implementing tile sliding logic

--- lib/widgets/tile_widget.dart
+++ lib/widgets/tile_widget.dart
@@ -4,3 +4,19 @@ import '../utils/game_colors.dart';
 
 class TileWidget extends StatelessWidget {
   final Tile tile;
+  final double tileSize;
+  final double padding;
+
+  const TileWidget({
+    super.key,
+    required this.tile,
+    required this.tileSize,
+    required this.padding,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AnimatedPositioned(
+      key: ValueKey(tile.id),
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.easeInOut,
