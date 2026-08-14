0651ae56011148225b8ffe7fae4dd8f516156c23
faguenou <faguenouz@gmail.com>
2026-08-14 20:32:10 +0100

    adding grid background styling

--- lib/utils/game_colors.dart
+++ lib/utils/game_colors.dart
@@ -16,3 +16,19 @@ class GameColors {
       case 64:
         return Colors.orange[500]!;
       case 128:
+        return Colors.orange[600]!;
+      case 256:
+        return Colors.orange[700]!;
+      case 512:
+        return Colors.orange[800]!;
+      case 1024:
+        return Colors.orange[900]!;
+      case 2048:
+        return Colors.deepOrange;
+      default:
+        return Colors.deepOrangeAccent;
+    }
+  }
+
+  static Color getTextColor(int value) {
+    return value <= 4 ? Colors.grey[800]! : Colors.white;
