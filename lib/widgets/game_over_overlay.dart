7dc614c0a3e220817f0ebcfaf41338f7c6ac61f4
lfarssi <medlfarssi10@gmail.com>
2026-08-14 07:56:10 +0100

    updating README with instructions

--- lib/widgets/game_over_overlay.dart
+++ lib/widgets/game_over_overlay.dart
@@ -11,3 +11,19 @@ class GameOverOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      color: Colors.white70,
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            const Text(
+              'Game Over!',
+              style: TextStyle(
+                fontSize: 42.0,
+                fontWeight: FontWeight.bold,
+                color: Colors.black87,
+              ),
+            ),
+            const SizedBox(height: 24.0),
+            ElevatedButton(
+              onPressed: onRestart,
