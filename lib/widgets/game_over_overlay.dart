a3e71dde2256cbecd0d1fc0b83d4bb48a17c98a5
faguenou <faguenouz@gmail.com>
2026-08-14 03:44:10 +0100

    improving game layout for mobile devices

--- lib/widgets/game_over_overlay.dart
+++ lib/widgets/game_over_overlay.dart
@@ -11,33 +11,3 @@ class GameOverOverlay extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.white70,
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text(
-              'Game Over!',
-              style: TextStyle(
-                fontSize: 42.0,
-                fontWeight: FontWeight.bold,
-                color: Colors.black87,
-              ),
-            ),
-            const SizedBox(height: 24.0),
-            ElevatedButton(
-              onPressed: onRestart,
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.orange[500],
-                foregroundColor: Colors.white,
-                padding: const EdgeInsets.symmetric(
-                    horizontal: 32.0, vertical: 16.0),
-              ),
-              child: const Text('Try Again', style: TextStyle(fontSize: 20)),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
--- lib/widgets/score_board.dart
+++ lib/widgets/score_board.dart
@@ -39,3 +39,6 @@ class ScoreBoard extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+}
